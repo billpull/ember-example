38bd48ffa66330beb402904b07a9c6a45b096482
billpull <bill.pull@gmail.com>
2016-01-26 22:57:48 -0500

    Change Component Properties

--- app/components/super-hero.js
+++ app/components/super-hero.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
 
+// app/components/super-hero.js
 export default Ember.Component.extend({
+  hero: null, // passed in
+  tagName: 'li',
+  classNames: ['super-hero'],
+  heroClass: function () {
+    var hero = this.get('hero');
+    if (!Ember.isNone(hero)) {
+      return Ember.String.dasherize(hero.get('heroName'));
+    }
+
+    return '';
+  }.property(),
+  classNameBindings: ['heroClass'],
+
+  actions: {
+    assemble: function () {
+      this.attrs.assemble(this.get('hero'));
+    }
+  }
 });
