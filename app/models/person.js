13505c59629680e882ea9391139f307c5b2feb01
billpull <bill.pull@gmail.com>
2016-01-26 21:53:46 -0500

    Setup Ember Project and Initial Routes

---
+++ app/models/person.js
@@ -0,0 +1,11 @@
+import Ember from 'ember';
+
+export default Ember.Object.extend({
+  // these will be supplied by `create`
+  firstName: null,
+  lastName: null,
+
+  fullName: Ember.computed('firstName', 'lastName', function() {
+    return `${this.get('firstName')} ${this.get('lastName')}`;
+  })
+});
--- app/router.js
+++ app/router.js
@@ -6,6 +6,9 @@ var Router = Ember.Router.extend({
 });
 
 Router.map(function() {
+  this.route('people', {path: '/'}, function() {
+    this.route('person', {path: '/:person_id'});
+  });
 });
 
 export default Router;
