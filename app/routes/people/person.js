2308a8ac32266fc7084eaefb18273032f4e6479c
billpull <bill.pull@gmail.com>
2016-01-26 21:56:00 -0500

    Create Single Person Template

--- app/routes/people/person.js
+++ app/routes/people/person.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
+import Person from '../../models/person';
 
 export default Ember.Route.extend({
+  model: function () {
+    return Person.create({
+      firstName: 'Steve',
+      lastName:  'Rogers'
+    });
+  }
 });
